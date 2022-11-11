540a290d8ebd9e876cddaf0f5bf7b3e9ee100d1f
alperenkolat <alperenklt24@gmail.com>
2022-11-11 16:18:30 +0300

    Veri kaybı önlendi ve basitleştirildi .

--- Clean_csv.py
+++ Clean_csv.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
@@ -6,25 +7,8 @@ from nltk.corpus import stopwords
 # Tek seferliğine stop worsds'leri indirdik
 # nltk.download('stopwords')
 
-# Csv okuyup dataframe şeklinde aldık
-dfeval = pd.read_csv(r"C:\Users\alper\OneDrive\Belgeler\PYTHON PROJECTS\yazlab2\rows.csv",dtype={'Consumer complaint narrative': 'str', 'Consumer consent provided?': 'str','Consumer disputed?':'str'})
-
-# Projede kullanacağımız gerekli Sütunları tanımladık
-required_col=["Product","Issue","Company","State","ZIP code","Complaint ID"]
-
-
-# Dataframe sütunlarını aldık
-col_name=dfeval.columns
-
-# Gerekli sütunları içinden çıkardık
-for i  in required_col:
-
-    col_name=col_name.drop(i)
-
-# Kalan gereksiz sütunları sildik
-for i in col_name:
-
-    dfeval.pop(i)
+# Csv sadece gerekli sutünları okuyup dataframe şeklinde aldık
+dfeval = pd.read_csv(r"rows.csv",usecols=[1,3,7,8,9,17], encoding="utf-8")
 
 
 # Boş olan satıları sildik
@@ -34,10 +18,11 @@ dfeval.dropna(inplace = True)
 # Kullanacağımız stopword kütüphanesini seçtik
 stop_word_list=stopwords.words('english')
 
-# Stop wordleri ve noktalama işaretlerini sildik
-for i in required_col[:3]:
-
+# Listedeki sutünlardan stop wordleri ve noktalama işaretlerini sildik ayrıca karakterleri küçülttük.
+required_col=["Product","Issue","Company"]
+for i in required_col:
     dfeval[str(i)]=dfeval[str(i)].str.replace("[^\w\s]","")
+    dfeval[str(i)] = dfeval[str(i)].apply(lambda x: ' '.join(x.lower() for x in x.split()))
     dfeval[str(i)] = dfeval[str(i)].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop_word_list)]))
 
 
